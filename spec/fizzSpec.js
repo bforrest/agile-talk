e345f22fa61d3d39456e2525aec7cfac34b567cb
Barry Forrest <bforrest30@gmail.com>
2017-07-30 20:25:26 -0500

    failing test for 3 returning Fizz

--- spec/fizzSpec.js
+++ spec/fizzSpec.js
@@ -3,9 +3,11 @@ describe("Fizz", function() {
 
     beforeEach(function() {
         fizz = new FizzParser();
-
     });
 
+    it('should be trure', function() {
+        expect(1).toEqual(1);
+    })
     it("should print 'Fizz' when the number is three (3)", function() {
         var result = fizz.Parse(3);
         expect(result).toEqual('three');
--- src/fizz.js
+++ src/fizz.js
@@ -1,3 +1,6 @@
-function FizzParser() {
+function FizzParser() {}
 
+FizzParser.prototype.Parse = function(value) {
+    if (value === 3)
+        return 'Fizz';
 }
